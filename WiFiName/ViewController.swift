4875005842a79c8948fa987b4db2150a2b6f8885
ChillarAnand <anand21nanda@gmail.com>
2019-08-13 18:51:49 +0530

    Initial Commit

---
+++ WiFiName/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiFiName
+//
+//  Created by anand on 8/13/19.
+//  Copyright © 2019 chillar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
