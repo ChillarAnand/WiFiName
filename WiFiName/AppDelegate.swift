b12494854454bd7b3e273b0cb7db85ea172c282d
ChillarAnand <anand21nanda@gmail.com>
2019-09-10 10:12:22 +0530

    Update SSID on network change

--- WiFiName/AppDelegate.swift
+++ WiFiName/AppDelegate.swift
@@ -11,35 +11,51 @@ import CoreWLAN
 
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    
+class AppDelegate: NSObject, NSApplicationDelegate, CWEventDelegate {
+
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     var wifiName: String? = nil
     let wifiClient = CWWiFiClient.shared()
-    
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        listenSSIDChanges()
         showWiFiName()
     }
-    
+
+    func listenSSIDChanges () {
+        wifiClient.delegate = self
+        do {
+            try wifiClient.startMonitoringEvent(with: .ssidDidChange)
+        } catch {
+
+        }
+    }
+
     func showWiFiName() {
         wifiName = getWiFiName()
         statusItem.button?.title = wifiName ?? "Not connected"
         statusItem.button?.target = self
         statusItem.button?.action = #selector(showSettings)
     }
-    
+
     func getWiFiName() -> String? {
         let interface = wifiClient.interface()
         return interface?.ssid()
     }
-    
+
+    func ssidDidChangeForWiFiInterface(withName interfaceName: String) {
+        DispatchQueue.main.async {
+            self.showWiFiName()
+        }
+    }
+
     @objc func showSettings() {
         let item: NSMenu = NSMenu()
         let quitItem = NSMenuItem(title: "Quit WiFiName", action: nil, keyEquivalent: "")
         item.addItem(quitItem)
-        
+
     }
-    
+
     @objc func quitWiFiName() {
         print("Quitting WiFiName")
     }
